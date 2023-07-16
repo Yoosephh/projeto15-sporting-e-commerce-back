00360d47c6cffdd6fad379713c5d5caa26273d24
Yoosephh <jose.es.oleg@gmail.com>
2023-07-16 14:59:50 -0300

    chore: Adjust for deploy on render

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const PORT = 5000;
-app.listen(PORT, () =>
-  console.log(`Tudo certo! Servidor rodando na porta ${PORT} :D`)
+const port = process.env.PORT || 5000
+app.listen(port, () =>
+  console.log(`Tudo certo! Servidor rodando na porta ${port} :D`)
 );
