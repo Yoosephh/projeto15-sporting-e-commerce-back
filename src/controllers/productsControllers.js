f1c0639330e4cc8c01b98884b6075ee4756aca7b
Yoosephh <jose.es.oleg@gmail.com>
2023-07-16 13:28:23 -0300

    Feature: Rota filtrar produtos

--- src/controllers/productsControllers.js
+++ src/controllers/productsControllers.js
@@ -14,3 +14,23 @@ export const getProducts = async (req, res) => {
   const products = await db.collection("products").find().toArray();
   res.send(products);
 };
+export async function filterProducts (req,res) {
+  const {type} = req.params
+  let productsFiltered;
+  try{
+    if(type === "brinquedos"){
+      productsFiltered = await db.collection("products").find({type:"brinquedos"}).toArray()
+    } else if (type === "roupas"){
+      productsFiltered = await db.collection("products").find({type:"roupas"}).toArray()
+    } else if (type === "calcados") {
+      productsFiltered = await db.collection("products").find({type:"calcados"}).toArray()
+    } else if(type === "acessorios"){
+      productsFiltered = await db.collection("products").find({type:"acessorios"}).toArray()
+    } else {
+      return res.status(404).send("Categoria indisponível!")
+    }
+    res.send(productsFiltered)
+  } catch(err) {
+    console.log(err)
+  }
+}
--- src/routes/products.Routes.js
+++ src/routes/products.Routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import {
   addProducts,
   getProducts,
+  filterProducts
 } from "../controllers/productsControllers.js";
 
 const productsRouter = Router();
 
 productsRouter.post("/produtos", addProducts);
 productsRouter.get("/produtos", getProducts);
+productsRouter.get("/produtos/:type", filterProducts)
 
 export default productsRouter;
